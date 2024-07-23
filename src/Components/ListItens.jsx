ec1530cee9938e0d03c5ca75d04ea630e2825c98
Ravel Silva <contatoravelsilva@gmail.com>
2024-07-22 22:41:00 -0300

    Update ListItens.jsx

--- src/Components/ListItens.jsx
+++ src/Components/ListItens.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./ListItens.css"
 const ListItens = ({ lista }) => {
   return (
-    <> {/*Sempre utilizar os fragments <> </> */}
-      {lista.map((item) => (
-        <p>{item}</p>
-      ))}
+     <>
+      {" "}
+      {/*Sempre utilizar os fragments <> </> */}
+      {lista.length > 0 ? (
+        lista.map((item) => <p>{item}</p>)
+      ) : (
+        <h2>"Sua lista está vazia"</h2>
+      )}
     </>
   );
 };
